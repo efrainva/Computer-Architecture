5d56ea7a3abf2a8acd116fe2b8eca36c335a6cff
efrain <efarinva98@gmail.com>
2020-05-12 19:49:31 -0400

    stack mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,46 +6,65 @@ class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
-        self.pc = 0
-        self.running = True
-        self.ram = [0] * 255
-        self.reg = []
-    def ram_read(self,id):
-        return self.ram[id]
+        self.pc = 0# the pointer
+        self.ram = [0] * 256
+        # register
+        self.reg = [0] * 8
+        self.reg[7] = 0xF4
+
+        self.sp = 7
+
+        self.opcodes = {
+            0b00000001: "HLT",
+            0b01000111: "PRN",
+            0b10000010: "LDI",
+            0b10100010: "MUL", 
+            0b01000101: "PUSH",
+            0b01000110: "POP", 
+            0b10100000: "ADD",
+
+        }   
 
 
-    def load(self):
-        
-        """Load a program into memory."""
+    def ram_read(self, address):
 
+        return self.ram[address]
+
+    def ram_write(self, address, item):
+        write = self.ram[address] = item
+        return write
+
+    def load(self, filename): 
         address = 0
+        try:
+            with open(filename) as file:
+                for line in file:
+                    fileSplit = line.split("#")
+                    numberString = fileSplit[0].strip()
 
-        # For now, we've just hardcoded a program:
+                    if numberString == "":
+                        continue
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+                    num = int(numberString, 2)
+                    self.ram[address] = num
+                    address += 1
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+        except FileNotFoundError:
+            sys.exit(2)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+            # self.pc += 3
+        elif op == "MUL":
+            self.reg[reg_a] += self.reg[reg_b]
+            # self.pc += 3
         else:
             raise Exception("Unsupported ALU operation")
+
+
 
     def trace(self):
         """
@@ -55,8 +74,6 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -68,30 +85,45 @@ class CPU:
         print()
 
     def run(self):
-        """Run the CPU."""
-        # pt = point in time
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        HLT = 0b00000001
-
-
-        bina = format(8,'b')
-
-        print('binary:',bina,'num input:',8)
-
+        print(self.pc)
+        self.running = True 
         while self.running:
-            command = program[self.pc]
+            IR = self.ram_read(self.pc)
+            print(self.pc)
+            #  print("This is what's not working", IR)
+            operandA = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            opcode = self.opcodes[IR]
+            if opcode == "HLT":
+                self.running = False
+            if opcode == "PRN":
+                print(self.reg[operandA])
+                self.pc += 2
+            if opcode == "LDI":
+                self.reg[operandA] = operand_b
+                self.pc += 3
 
-            print(command)
-            self.pc +=1
 
-            if command == HLT:
-                break
+
+            if opcode == "ADD":
+                self.reg[operandA] += self.reg[operand_b]
+                self.pc += 3
+
+            if opcode == "MUL":
+                self.reg[operandA] * self.reg[operand_b]
+                self.pc += 3
+
+
+            if opcode == "PUSH":
+                self.reg[self.sp] -= 1
+                operandA = self.ram[self.pc + 1]
+                item = self.reg[operandA]
+                self.ram[self.reg[self.sp]] = item
+                self.pc +=2
+
+            if opcode == "POP":
+                operandA = self.ram[self.pc + 1]
+                item = self.ram[self.reg[self.sp]]
+                self.reg[operandA] = item 
+                self.reg[self.sp] += 1
+                self.pc += 2
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -7,7 +7,6 @@ from cpu import *
 
 cpu = CPU()
 
-cpu.load()
+cpu.load(sys.argv[1])
 cpu.run()
 
-# read the file 
